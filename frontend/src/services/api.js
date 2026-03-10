8a6aacc47dbbc2823c056ca617e2096ac717890a
Niranjan <niranjanpandey2484@gmail.com>
2026-03-10 17:27:10 +0530

    fix frontend API to use azure backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://quickresume-backend-niranjan-dtfeebb8c0bba3cc.centralindia-01.azurewebsites.net"
+  baseURL: "https://quickresume-backend-niranjan-dtfeebb8c0bba3cc.centralindia-01.azurewebsites.net/api"
 });
 
 export default API;
