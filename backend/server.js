791e5fc16df259f6e8c2f1ee35311f8b39dbe5ba
Niranjan <niranjanpandey2484@gmail.com>
2026-03-11 15:49:09 +0530

    Fix uploads folder creation for Azure

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,15 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const fs = require("fs");
+const path = require("path");
+
+const uploadDir = path.join(__dirname, "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
 const app = express();
 
 // middleware
